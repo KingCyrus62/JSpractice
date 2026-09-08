0c93db6447afd206f754bbcbe14e6bd7f19af6f1
IfeOluwa <ifeoluwajohnson027@gmail.com>
2026-09-08 19:42:13 +0100

    Start practice on JavaScript

---
+++ script.js
@@ -0,0 +1,46 @@
+/*console.log(`Hello World!`);
+window.alert("This is an alert!");
+
+document.getElementById("Myh1").textContent="Hello";
+document.getElementById("Myp").textContent="I like Pizza!";
+let x=100;
+
+console.log(age);
+let price = 10.99;
+console.log(price);
+let gpa = 4.00;
+console.log(`Your are ${age} years old.`);
+console.log(`Your GPA is ${gpa}.`);
+console.log(`The price is ${price}`);
+console.log(typeof age);
+let firstName = "Johnson";
+console.log(typeof firstName);
+console.log(`My name is ${firstName}.`);
+let faveFood = "Pizza";
+console.log(`My favorite food is ${faveFood}.`);    
+let email = "johnson123@email.com";
+console.log(`My email is ${email}.`);
+let online = true;
+console.log(typeof online);
+console.log(`Bro is online: ${online}`);
+let isStudent = true;
+console.log(`Enrolled? ${isStudent}`); */
+
+/*let fullName = "Johnson IfeOluwa";
+let age = 17;
+let student = true;
+document.getElementById("P1").textContent = `Your name is ${fullName}.`;
+document.getElementById("P2").textContent = `You are ${age} years old.`;
+document.getElementById("P3").textContent = `Are you a student? ${student}`;
+let studentSize = 25;
+studentSize = studentSize % 2;
+console.log(`The student size is ${studentSize}.`);*/
+
+
+/*let userName = window.prompt("What's ur Username?");
+console.log(`Hello ${userName}.`);*/
+let username;
+document.getElementById("submitBtn").onclick = function() {
+    username = document.getElementById("UserName").value;
+    document.getElementById("Myh1").textContent = `Hello ${username}.`;
+}
